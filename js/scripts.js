157e44da73ac8297e6d72c3bacc55302e1adbfae
O. J. Wilcox <297811+oj-codes@users.noreply.github.com>
2021-07-31 15:38:31 -0400

    adjust whitespace and fix nav links

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $element = $('.main-header'),
 className = 'box-shadow';
 
 $document.scroll(function() {
-    if ($document.scrollTop() >= 20) {
+    if ($document.scrollTop() >= 50) {
     // Change 50 to the value you require
     // for the event to trigger
         $element.addClass(className);
