c839f8e7852895faa3a32e243f87e285aa50ed00
O. J. Wilcox <297811+oj-codes@users.noreply.github.com>
2021-07-31 15:50:25 -0400

    manage active state in nav, add whitespace

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,8 @@ $document.scroll(function() {
         $element.removeClass(className);
     }
 });
+
+$(".nav .nav-link").on("click", function(){
+    $(".nav").find(".active").removeClass("active");
+    $(this).addClass("active");
+});
