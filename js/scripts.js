ea7cc2fd2e94c1c838bb154e42afa587204e1b4b
O. J. Wilcox <297811+oj-codes@users.noreply.github.com>
2021-07-31 15:24:33 -0400

    add dropshadow on scroll to navbar

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var $document = $(document),
+$element = $('.main-header'),
+className = 'box-shadow';
+
+$document.scroll(function() {
+    if ($document.scrollTop() >= 20) {
+    // Change 50 to the value you require
+    // for the event to trigger
+        $element.addClass(className);
+    } else {
+        $element.removeClass(className);
+    }
+});
